136430263d369fbd72450ba96ecfa9eef9be6d31
Stefan <fanja1911@gmail.com>
2023-09-24 20:37:50 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Общение с пользователем
+
+
+// alert("Hello!");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+const answer = +prompt("Are you 18?", "18");
+console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt("WHAT'S YOUR NAME?", "");
+answers[1] = prompt("WHAT'S YOUR SURNAME?", "");
+answers[2] = prompt("WHAT'S YOUR AGE?", "");
+
+console.log(typeof (answers));
+
