e5fb0f441d796db25d447750fbbb5638ffef77e7
Stefan <fanja1911@gmail.com>
2023-10-25 17:30:15 +0300

    new tasks

---
+++ js/ООП.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let str = "some";
+let strObj = new String(str);
+
+console.log(typeof str); // string
+console.log(typeof strObj); // object
+
+console.dir([1, 2, 3]);
+
+
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function () {
+        console.log("Hello");
+    }
+};
+
+// Создание прототипа
+const john = Object.create(soldier);
+
+john.sayHello(); // Hello
